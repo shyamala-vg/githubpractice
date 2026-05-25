c31377476373b6166e2a6f89a6870136f00040af
Shyamala Gowri <shyamalagowri.vg@wipro.com>
2026-05-25 15:21:27 +0530

    Fixing the test error

--- app/main.py
+++ app/main.py
@@ -6,3 +6,4 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -8,3 +8,4 @@ def test_home():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+    
